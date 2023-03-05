8949384e977eb7a0eb409f351c3e917ab6169020
victorbenedict <103329136+victorbenedict@users.noreply.github.com>
2023-03-06 07:06:24 +0800

    Provide solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, numOfRepeat) {
+	result = "";
+	if (numOfRepeat < 0){return 'ERROR';}
+	else{
+		for (index = 0; index < numOfRepeat; index ++){
+			result += word;
+			}
+		}
+	return result;
 };
 
 // Do not edit below this line
