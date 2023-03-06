df155faf103b45f399468a54607cea81c4892474
victorbenedict <103329136+victorbenedict@users.noreply.github.com>
2023-03-06 09:10:30 +0800

    Provide solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,11 @@
-const reverseString = function() {
+const reverseString = function(word) {
+let newWord = "";
+const wordlen = word.length - 1;
+for(let index = wordlen; index >= 0 ; index--){
+	newWord += word[index];
+	}
+	  
+return newWord;
 
 };
 
