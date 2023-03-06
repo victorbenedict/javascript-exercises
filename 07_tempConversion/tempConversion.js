b5611a096ec6172b23e5a127e588c93c9ce668ce
victorbenedict <103329136+victorbenedict@users.noreply.github.com>
2023-03-06 15:54:07 +0800

    Provide solution

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(F) {
+  temp = (F-32) * (5 / 9);
+  if(Number.isInteger(temp)){ return temp;}
+  else{ return Number.parseFloat(temp.toFixed(1));}
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(C) {
+  temp = C * (9 / 5) + 32;
+  if(Number.isInteger(temp)){ return temp;}
+  else{ return Number.parseFloat(temp.toFixed(1));}
 };
 
 // Do not edit below this line
