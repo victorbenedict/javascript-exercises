f13160508a2a3e0fb9420a671ad4b7761c3dd1e9
victorbenedict <103329136+victorbenedict@users.noreply.github.com>
2023-03-06 13:39:37 +0800

    Provide solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
 
+	for(argcount = 0; argcount < args.length; argcount++){
+		for (let index = arr.length; index > 0; index--){
+			let elementValue = arr[index-1];
+			let currentArrayIndex = index -1;
+			if(elementValue === args[argcount]){ // Check if the elements is equal to the value of the argument
+				arr.splice(currentArrayIndex,1); //splice the element from the array
+
+			}
+		}
+	}
+	return arr;
 };
 
 // Do not edit below this line
