fd015814bdb8b351a7a6922bd2083232a6de3214
victorbenedict <103329136+victorbenedict@users.noreply.github.com>
2023-03-06 14:12:25 +0800

    Provide solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(number_A,number_B) {
 
+	let total = 0;
+	
+
+	if(	number_A < 0 || 
+		number_B < 0 || 
+		typeof(number_A) != "number" ||
+		typeof(number_B) != "number"	
+		){return 'ERROR'} //ERROR checking for negative numbers or non-number parameters.
+
+	//first integer with the lower value as start and last integer with the higher value is the end of the calculation > 
+	if(number_A > number_B){ 
+		for(count = number_B; count <= number_A; count++){ //Loop will be created to calculate the sum in between the first and last number.
+			total += count;
+		}
+	}
+	else{
+		for(count = number_A; count <= number_B; count++){
+			total += count;
+		}
+	}
+	return total;
 };
 
 // Do not edit below this line
