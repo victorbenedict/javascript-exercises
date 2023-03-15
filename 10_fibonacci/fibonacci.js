0fab1b772717ad78afa918a3b12514bd16cb1349
victorbenedict <103329136+victorbenedict@users.noreply.github.com>
2023-03-15 19:30:53 +0800

    Provide solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+	let arr = [0,1,1,2,3];
 
-};
+	if(num < 5 && num >= 0){return arr[num]}
+	else if(num < 0){ return 'OOPS'}
+	else{
+		for(i = 5; i <=num; i++){
+			arr.push(arr[arr.length - 2] + arr[arr.length - 1])
+		}
+		return arr[arr.length - 1]
+	}};
 
 // Do not edit below this line
 module.exports = fibonacci;
