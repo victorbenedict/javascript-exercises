63cc0fa7d9a4d6746c43fc303854af80fca555ae
victorbenedict <103329136+victorbenedict@users.noreply.github.com>
2023-03-15 12:51:42 +0800

    Provide solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+	let newStr = str.replace(/[^\w]/gi, "").toLowerCase();
+	const reversedStr = newStr.split("").reverse().join("");
+	return newStr == reversedStr;
 };
 
 // Do not edit below this line
